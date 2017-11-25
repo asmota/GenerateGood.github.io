3429a11cb5b2a6568f3e729928164ac786e3668c
asmota <32639144+asmota@users.noreply.github.com>
2017-11-25 15:28:28 +0000

    Ashley changes 25-Nov
    White background, remove photos, new banner, nearly finished :)

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 var quotes = [
-'Create a holiday for someone you appreciate and send them a card',
-'Send a postcard to a sick child through sendkidstheworld',
-'Make someone a CD with all the #1 Billboard Hits on their birthday week',
-'Take flowers to the nursing (firefighting/police) station to say thank you',
-'Start an email address for someone you love and send it emails with your favourite photos/some thoughts'
+'Create a holiday for someone you appreciate and send them a card.',
+'Send a postcard to a sick child through <a href="http://www.sendkidstheworld.com/index.php/send-post-cards.html" target="_blank">Send Kids The World</a>.',
+'Make someone a CD with all the #1 Billboard Hits on their birthday week.',
+'Take flowers to the nursing (firefighting/police) station to say thank you.',
+'Start an email address for someone you love and send it emails with your favourite photos/some thoughts.'
 ]
 
 function newIdea() {
